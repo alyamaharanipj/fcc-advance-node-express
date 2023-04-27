89773f557af015ac090f1c2aa9b70b5eb47aa56c
cst_dev180 <cst_dev180@unitedtractors.com>
2023-04-28 00:51:33 +0700

    feat: user object serialization

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const session = require('express-session');
 const passport = require('passport');
+const { ObjectID } = require('mongodb');
 
 const app = express();
 app.set('view engine', 'pug');
@@ -32,3 +33,12 @@ app.listen(PORT, () => {
 
 passport.initialize();
 passport.session();
+passport.serializeUser((user, done) => {
+  done(null, user._id);
+});
+
+passport.deserializeUser((id, done) => {
+  // myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+    done(null, null);
+  // });
+});
