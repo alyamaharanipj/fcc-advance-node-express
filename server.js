2c8809dc896776f85b694c759c2afbea7d080965
cst_dev180 <cst_dev180@unitedtractors.com>
2023-04-28 10:26:35 +0700

    feat: new middleware

--- server.js
+++ server.js
@@ -44,6 +44,19 @@ myDB(async client => {
     }
   );
 
+  function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next();
+    }
+    res.redirect('/');
+  };
+
+  app
+  .route('/profile')
+  .get(ensureAuthenticated, (req,res) => {
+      res.render('profile');
+  });
+
   // Serialization and deserialization here...
   passport.initialize();
   passport.session();
