8dca2e287c3604e0ab40f2608dc7758f331a4f29
cst_dev180 <alyamaharanipj@gmail.com>
2023-04-28 21:36:01 +0700

    feat: announce new user

--- public/client.js
+++ public/client.js
@@ -1,7 +1,11 @@
 $(document).ready(function () {
   let socket = io();
-  socket.on('user count', function (data) {
-    console.log(data);
+  socket.on('user', data => {
+    $('#num-users').text(data.currentUsers + ' users online');
+    let message =
+      data.username +
+      (data.connected ? ' has joined the chat.' : ' has left the chat.');
+    $('#messages').append($('<li>').html('<b>' + message + '</b>'));
   });
   // Form submittion with new message in field with id 'm'
   $('form').submit(function () {
