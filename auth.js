66a3721459dff78a13c509d3faa509fc3a2e3ef7
cst_dev180 <alyamaharanipj@gmail.com>
2023-04-28 20:17:14 +0700

    feat: social auth 2

--- auth.js
+++ auth.js
@@ -2,6 +2,7 @@ const passport = require('passport');
 const { ObjectID } = require('mongodb');
 const LocalStrategy = require('passport-local');
 const bcrypt = require("bcrypt")
+const GitHubStrategy = require('passport-github').Strategy;
 
 module.exports = function (app, myDataBase) {
   // Serialization and deserialization here...
@@ -16,6 +17,17 @@ module.exports = function (app, myDataBase) {
       return done(null, user);
     });
   }));
+    
+  passport.use(new GitHubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: 'https://fcc-advance-node-express.alyamaharanipj.repl.co/auth/github/callback'
+  },
+    function(accessToken, refreshToken, profile, cb) {
+      console.log(profile);
+      //Database logic here with callback containing your user object
+    }
+  ));
 
   passport.serializeUser((user, done) => {
     done(null, user._id);
--- server.js
+++ server.js
@@ -38,6 +38,3 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log('Listening on port ' + PORT);
 });
-
-
-
